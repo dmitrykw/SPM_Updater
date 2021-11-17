33ce45157b97ab02066d38ac67e9de36345877c2
Dmitry Koziakov <33961009+dmitrykw@users.noreply.github.com>
2021-11-17 17:49:15 +0300

    Added arguments checking

--- Visual_Updater/updater/updater/App.xaml.cs
+++ Visual_Updater/updater/updater/App.xaml.cs
@@ -13,5 +13,13 @@ namespace updater
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            if (e.Args.Length < 1 || e.Args[0] != "update")
+            {
+                MessageBox.Show("This is a part of SPM Monitoring Application. Do not launch this module directly. It will take no effect.", "Updater Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                Environment.Exit(0);
+            }
+        }
     }
 }
